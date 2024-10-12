4ecd313e86ba4e5329a17132689bb789d4e75d7e
AbrahamBarragan <abraham.barragan02@gmail.com>
2024-10-12 09:31:24 -0700

    Configurando entidad para mongoDB

--- CinemaKino/Dato.cs
+++ CinemaKino/Dato.cs
@@ -1,25 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
 
 namespace CinemaKino
 {
     internal class Dato
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
+
+        [BsonElement("firstname")]
         public string FirstName { get; set; }
+
+        [BsonElement("lastname")]
         public string LastName { get; set; }
+
+        [BsonElement("email")]
         public string Email { get; set; }
+
+        [BsonElement("phone")]
         public string Phone { get; set; }
+
+        [BsonElement("gender")]
         public string Gender { get; set; }
+
+        [BsonElement("moviegenres")]
         public string MovieGenres { get; set; }
+
+        [BsonElement("movietitle")]
         public string MovieTitle { get; set; }
+
+        [BsonElement("date")]
         public DateOnly Date { get; set; }
+
+        [BsonElement("time")]
         public TimeOnly Time { get; set; }
+
+        [BsonElement("price")]
         public decimal Price { get; set; }
+
+        [BsonElement("seat")]
         public int Seat { get; set; }
+
+        [BsonElement("cinemaroom")]
         public int CinemaRoom { get; set; }
+
+        public Dato()
+        {
+            string connectionString = "";
+        }
     }
 }
